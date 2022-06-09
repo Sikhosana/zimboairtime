ecd80fd5a2a83e7b4480f9e99f334a191ecb144e
Sikhosana Dhliwayo <sdhliwayo01@gmail.com>
2022-06-09 03:14:03 -0600

    initial commit

---
+++ src/main/java/com/upheaval/zimboairtime/ZimboairtimeApplication.java
@@ -0,0 +1,13 @@
+package com.upheaval.zimboairtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZimboairtimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZimboairtimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upheaval/zimboairtime/ZimboairtimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upheaval.zimboairtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZimboairtimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
