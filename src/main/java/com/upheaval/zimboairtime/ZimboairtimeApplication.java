24d4f157e5ad850ffbb736c8047ec2412293bbdb
sikhosana <sdhliwayo01@gmail.com>
2022-06-20 12:56:41 -0600

    enabled swagger documentation api

--- src/main/java/com/upheaval/zimboairtime/ZimboairtimeApplication.java
+++ src/main/java/com/upheaval/zimboairtime/ZimboairtimeApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ZimboairtimeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ZimboairtimeApplication.class, args);
     }
+
+
 
 }
